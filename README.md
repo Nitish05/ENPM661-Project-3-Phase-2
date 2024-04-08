65e065c10f7dfae37adc0adcc3b4151179437339
Nitish05 <rrnitish@gmail.com>
2024-04-07 20:02:52 -0400

    Refactor code for better readability and maintainability in test3.py and test2.py

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
-# ENPM661-Project-3-Phase-2
+# ENPM-661-Project-3-A_Star-Phase 2
+
+This repository contains the code for Project 3 Phase 2 of the ENPM 661 course. The project focuses on implementing the A* algorithm for path planning in a given environment.
+
+## Installation
+
+To use this code, you need to have Python installed on your system. You can download Python from the official website: [Python.org](https://www.python.org/).
+
+## Dependencies
+- Python 3.7
+- OpenCV
+- Numpy
+- Queue
+- Time
+- Rclpy
+- Rclpy.node
+- geometry_msgs
+
+## Usage
+
+1. Clone the repository to your local machine:
+
+    ```bash
+    git clone https://github.com/Nitish05/ENPM661-Project-3-Phase-2.git
+    ```
+
+2. Navigate to the project directory:
+
+    ```bash
+    cd ENPM-661-Project-3-A_Star
+    ```
+
+3. Run the main script:
+
+    ```bash
+    python a_star_nitish_pranav.py
+    ```
+
+4. Follow the on-screen instructions to input the start and goal positions, and the program will generate the optimal path using the A* algorithm.\
+    eg:
+    ```bash
+    _____________________________________
+        __          __
+        / |       /    )
+    ---/__|-------\------_/_----__---)__-
+      /   |        \     /    /   ) /   )
+    _/____|____(____/___(_ __(___(_/_____
+
+
+    Step size should be between 1 and 10
+    Enter the step size: 10
+    Clearance distance should be a positive number
+    Enter the clearance distance: 5
+    Robot radius should be a positive number
+    Enter the robot radius: 5
+
+    Generating the map...
+
+    The start node and goal node should be within the canvas dimensions (11-1190, 11-490) and not inside an obstacle.
+
+    Enter the start node X: 11
+    Enter the start node Y: 11
+    Enter the start node Angle: 0
+    Enter the goal node X: 1000
+    Enter the goal node Y: 250
+    Enter the goal node Angle: 180
+    Start Node:  (11, 11, 0)
+    Goal Node:  (1000, 250, 180)
+
+    Running A* algorithm...
+    Goal Reached
+    Cost to Goal:  1310
+    Execution time: 13.1537 seconds
+    ```
+5.  
+    - The script will prompt the user to enter the step size, clearance distance, and robot radius.
+    - The script will take a few seconds to generate the map.
+    - The script will prompt the user to enter the start and goal coordinates as well as the orientation in the start and goal node. The origin (0,0) is at the bottom left corner of the grid space.
+    - The orientation is such that 0 degrees is along the positive y-axis, 90 degrees is along the positive x-axis, 180 degrees is along the negative y-axis, and 270 degrees is along the negative x-axis. (Like a clock face or compass directions.)
+    - The orientation should in 30 degrees increments. (0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330 ...)
+    - If the orientation is not in 30 degrees increments, the script will choose the nearest orientation in 30 degrees increments. eg: 127 will be rounded to 120.
+    - If the start and goal coordinates are valid, the script will display the path from the start to the goal using the A* algorithm.
+    - If the start and goal coordinates are invalid, the script will prompt the user to enter the coordinates again.
+    - The script will also display the time taken to find the path and the cost of the path.
+    - The script will also display the path in the map.
+    - For some start and goal coordinates, the script may not find a path. In such cases, the script will display a message saying "No path found".
+    - The white cells represent the free space, the black cells represent the obstacles, the gray cells represent the clearance + the bloated region for the robot radius, the red cell represents the start node, the green cell represents the goal node, the blue cells represent the nodes visited, and the red cells represent the path.
+    - The script will also save the path and the node creation as a video named A_star.mp4 in the current directory.
+    - In the video, the start node is displayed in red, the goal node is displayed in green, the nodes visited are displayed in blue, and the path is displayed in red.
+    - The script should not take more than 5 minutes to find the path.
+
+## Contributors
+Nitish Ravisankar Raveendran - rrnitish - 120385506\
+Pranav ANV - anvpran - 1204886110
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more information.
+
+## Acknowledgements
+
+Special thanks to the ENPM 661 course instructors for providing the project requirements and guidance.
--- maybe.py
+++ maybe.py
@@ -5,9 +5,9 @@
 import cv2
 import numpy as np
 from queue import PriorityQueue
-import matplotlib.pyplot as plt
 import time
-USE_ROS =False
+
+USE_ROS = False
 
 print('''
 _____________________________________
@@ -19,6 +19,7 @@ _/____|____(____/___(_ __(___(_/_____
                                      
 ''')
 
+# Get the mode from the user
 while True:
     mode = input("Enter the mode (1 for Gazebo Mode, 2 for 2D Mode): ")
     if mode in ['1', '2']:
@@ -28,12 +29,12 @@ while True:
 if mode == '1':
     USE_ROS = True
 
+
 if USE_ROS:
-    try:
+    try:  # Check if ROS2 libraries are installed
         import rclpy
-        from rclpy.node import Node
+        from rclpy.node import Node                  # Import the necessary ROS2 libraries
         from geometry_msgs.msg import Twist
-        # Any other ROS imports
         ROS_IMPORTED = True
     except ImportError:
         print("ROS2 libraries are not installed. Gazebo mode will not function.")
@@ -43,25 +44,23 @@ else:
     ROS_IMPORTED = False
     mode = '2'
 
-WhR = 3.3
-K = (2*np.pi*WhR)/60
-L = 28.7
-
+WhR = 3.3            # Wheel radius in cm
+K = (2*np.pi*WhR)/60 # Conversion factor from RPM to cm/s
+L = 30.6             # Wheelbase in cm
 canvas_height = 200
 canvas_width = 600
-# robo_radius = 22.0
-
 clearance_color = (127, 127, 127)
 obstacle_color = (0, 0, 0)
 free_space_color = (255, 255, 255)
-threshold = 1.0
+threshold = 1.5     # Threshold distance to check if the goal is reached
 
+# Intiate the publisher only if the ROS2 libraries are imported
 if ROS_IMPORTED:
     class VelocityPublisher(Node):
         def __init__(self):
             super().__init__('velocity_publisher')
             self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
-            time.sleep(2)  # Give some time for the publisher to set up
+            time.sleep(2)  
 
         def publish_velocity(self, linear_velocity, angular_velocity):
             msg = Twist()
@@ -73,6 +72,7 @@ if ROS_IMPORTED:
 # Initialize a white canvas
 canvas = np.ones((canvas_height, canvas_width, 3), dtype="uint8") * 255
 
+# Function to check if the node is an obstacle
 def obstacles(node):
     x, y = node
     Circ_center = (420, 120)
@@ -86,13 +86,13 @@ def obstacles(node):
     ]
     return any(obstacles)
 
+# Function to check if the node is within the clearance zone
 def clearance(x, y, clearance):
     clearance = clearance + robo_radius
     Circ_center = (420, 120)
     R = 60 + clearance
     Xc, Yc = Circ_center
     y = abs(y - canvas_height)
-    
     clearance_zones = [
         (x >= 150 - clearance and x <= 175 + clearance and y <= 200 + clearance  and y >= 100 - clearance),
         (x >= 250 - clearance and x <= 275 + clearance and y <= 100 + clearance and y >= 0 - clearance),
@@ -102,54 +102,60 @@ def clearance(x, y, clearance):
     return any(clearance_zones)
 
 
-
-def is_free(x, y):
+# Function to check if the node is free
+def is_free(x, y, theta):
     x = int(round(x))
     y = int(round(y))
+    theta = round(theta/30)*30
+    theta_normalized = theta % 360
+    theta_index = int(theta_normalized // 30)
     if x >= 0 and x < canvas_width and y >= 0 and y < canvas_height:
-        if canvas_array[x, y] == 0:
+        if canvas_array[x, y, theta_index] == 0:
             return True
     else:
         return False
 
-    
+# Function to get the neighbors of a node    
 def get_neighbors(node):
     dt = 0.1
-    neighbours = []
+    neighbours = [] # List to store the neighbors
     initial_x, initial_y, initial_theta = node
     # Convert initial orientation to radians for calculation
     initial_theta_rad = np.deg2rad(initial_theta)
-    action_set = [(0, RPM1), (RPM1, 0), (RPM1, RPM1), (0, RPM2), (RPM2, 0), (RPM2, RPM2), (RPM1, RPM2), (RPM2, RPM1)]
-    for action in action_set:
-        X_new, Y_new, thetan = initial_x, initial_y, initial_theta_rad
+    action_set = [(0, RPM1), (RPM1, 0), (RPM1, RPM1), (0, RPM2), (RPM2, 0), (RPM2, RPM2), (RPM1, RPM2), (RPM2, RPM1)] # Action set
+    # Example weight for actions, adjust based on your criteria
+    action_weights = {(0, RPM1): 1, (RPM1, 0): 1.2, (RPM1, RPM1): 1, (0, RPM2): 1.1, (RPM2, 0): 1.2, (RPM2, RPM2): 1, (RPM1, RPM2): 1.3, (RPM2, RPM1): 1.3}
 
-        vel = []
+    for action in action_set: 
+        X_new, Y_new, thetan = initial_x, initial_y, initial_theta_rad
+        vel = []  # List to store the velocities
         Vl = action[0] * K
         Vr = action[1] * K
         t = 0  # Reset t for each action
         while t < 1:
             t += dt
-  
             Xs = X_new
             Ys = Y_new
-            Vn = 0.5 * (Vl + Vr)
-            AVn = (Vr - Vl) / L
+            Vn = 0.5 * (Vl + Vr)  # Calculate the linear velocity in cm/s
+            AVn = (Vr - Vl) / L   # Calculate the angular velocity in rad/s
             vel.append((Vn, AVn))            
-            X_new += Vn * np.cos(thetan) * dt
-            Y_new += Vn * np.sin(thetan) * dt
-            thetan += AVn* dt
-       
-            if is_free(X_new, Y_new):
-                cv2.line(canvas, (int(round(Xs)), int(round(Ys))), (int(round(X_new)), int(round(Y_new))), (255, 0, 0), 1)
+            X_new += Vn * np.cos(thetan) * dt               # Calculate the new X coordinate in Cm
+            Y_new += Vn * np.sin(thetan) * dt               # Calculate the new Y coordinate in Cm
+            thetan += AVn* dt                               # Calculate the new orientation
+            if is_free(X_new, Y_new, 0): # Check if the new node is free
+                cv2.line(canvas, (int(round(Xs)), int(round(Ys))), (int(round(X_new)), int(round(Y_new))), (255, 0, 0), 1) # Draw the path
         thetan_deg = np.rad2deg(thetan) % 360
-        if is_free(X_new, Y_new):
-            cost = ((initial_x - X_new)**2 + (initial_y - Y_new)**2)**0.5
+        thetan_degt = round(np.rad2deg(thetan) / 30) * 30 % 360
+
+        if is_free(X_new, Y_new, thetan_degt):
+            weight = action_weights[action]
+            cost = ((initial_x - X_new)**2 + (initial_y - Y_new)**2)**0.5  # Calculate the cost
+            cost = cost * weight
             neighbours.append(((X_new,Y_new, thetan_deg), cost, vel))
-           
-        
     return neighbours
 
-def check_goal_reached(current_node, goal):
+# Function to check if the goal is reached
+def check_goal_reached(current_node, goal):   # Not checking the orientation because of non holonomic constraints
     distance = ((current_node[0] - goal[0]) ** 2 + (current_node[1] - goal[1]) ** 2) ** 0.5
     return distance < threshold
 
@@ -172,7 +178,8 @@ def a_star(start, goal):
             
         for next_node, cost, action in get_neighbors(current_node[0]):  # Get the neighbors of the current node
             cost_to_go = ((goal[0] - next_node[0])**2 + (goal[1] - next_node[1])**2)**0.5
-            theta_normalized = next_node[2] % 360
+            tnt = round(next_node[2]/30)*30
+            theta_normalized = tnt % 360
             theta_index = theta_normalized // 30
             new_cost = current_node[1] + cost + cost_to_go
             nc = current_node[1] + cost
@@ -180,19 +187,18 @@ def a_star(start, goal):
                 cost_so_far[next_node] = nc  # Update the cost so far
                 priority = new_cost   # Calculate the priority
                 pq.put((priority, (next_node, nc)))   # Add the node to the priority queue
-                canvas[int(round(next_node[1])), int(round(next_node[0]))] = (255, 0, 0)
-                canvas_array[int(round(next_node[0])), int(round(next_node[1]))] = np.inf
+                canvas_array[int(round(next_node[0])), int(round(next_node[1])), int(theta_index)] = np.inf # Update the canvas array
                 came_from[next_node] = (current_node[0], action)
                 count += 1
-                if count%10 == 0:  
-                    # cv2.resize(canvas, (600, 200)) 
-                    canvas_resized = cv2.resize(canvas, (1200, 400), interpolation=cv2.INTER_AREA)                 
-                    out.write(canvas_resized)
+                if count%1000 == 0:  
+                    # canvas_resized = cv2.resize(canvas, (1200, 400), interpolation=cv2.INTER_AREA)                 
+                    out.write(canvas)
     return None, None, None  # Return None if no path is found
 
+# Function to reconstruct the path
 def reconstruct_path(came_from, start, goal):
     current = goal
-    path_with_velocities = [(current, came_from[current][1] if current in came_from else (0, 0))]
+    path_with_velocities = [(current, came_from[current][1] if current in came_from else (0, 0))] # List to store the path with velocities
     while current != start:
         # Retrieve the current node and the velocities used to reach it.
         node_info = came_from[current]
@@ -204,36 +210,33 @@ def reconstruct_path(came_from, start, goal):
     
     return path_with_velocities
 
+# Function to visualize the path
 def visualize_path(path):
-    V = []
+    V = [] # List to store the velocities
     for i in range(len(path) - 1):
         current_node, velocities = path[i]
-        next_node = path[i + 1][0]
         x, y, t = current_node
-        xn, yn, tn = next_node
+        x_new = x
+        y_new = y
         for linear_vel, angular_vel in velocities:
-            V.append((linear_vel/100, -angular_vel))
-            cv2.arrowedLine(canvas, (int(round(x)), int(round(y))), (int(round(xn)),int(round(yn))), (0, 0, 255), 1)
-            canvas_resized = cv2.resize(canvas, (1200, 400), interpolation=cv2.INTER_AREA)
-            out.write(canvas_resized)
+            V.append((linear_vel/100, -angular_vel)) # Extract the linear and angular velocities and convert them to m/s and rad/s
+            tn = t
+            xa = x_new
+            ya = y_new
+            x_new += linear_vel * np.cos(np.deg2rad(tn)) * 0.1 
+            y_new += linear_vel * np.sin(np.deg2rad(tn)) * 0.1
+            tn += angular_vel * 0.1
+            cv2.line(canvas, (int(round(xa)), int(round(ya))), (int(round(x_new)), int(round(y_new)),), (0, 0, 255), 1) # Draw the path
+            # canvas_resized = cv2.resize(canvas, (1200, 400), interpolation=cv2.INTER_AREA)
+            out.write(canvas)
     cv2.destroyAllWindows()       
-    for i in range(30):
-        out.write(canvas_resized)
+    # for i in range(30):
+    #     out.write(canvas_resized)
+    canvas_resized = cv2.resize(canvas, (1200, 400), interpolation=cv2.INTER_AREA)
     cv2.imshow('Path', canvas_resized)
     return V
 
-
-
-# while True:
-#     print("Do you want procede to Gazebo mode or 2D mode?\n")
-#     print("1. Gazebo Mode.")
-#     print("2. 2D Mode.")
-#     mode = input("Enter the mode: ")
-#     if mode in ['1', '2']:
-#         break
-#     else:
-#         print("Invalid mode. Please enter a valid mode.")
-
+# Function to get the clearance distance and robot radius
 def get_clearance():
     while True:
         print("Clearance distance should be a positive number")
@@ -250,6 +253,7 @@ def get_clearance():
             break
     return clearance_distance, robo_radius
 
+# Function to generate the map
 def generate_map():
     print("\nGenerating the map...\n")
 
@@ -261,7 +265,8 @@ def generate_map():
             if obstacles((x, y)):
                 canvas[y, x] = obstacle_color
 
-    canvas_array = np.zeros((canvas_width, canvas_height))
+    # Create a 2D array to store the map
+    canvas_array = np.zeros((canvas_width, canvas_height, 12))
     for x in range(canvas_width):
         for y in range(canvas_height):
             if all(canvas[y, x] != free_space_color):
@@ -270,6 +275,7 @@ def generate_map():
                 canvas_array[x, y] = 0
     return canvas, canvas_array
 
+# Function to get the start node
 def get_start(): 
     C = clearance_distance + robo_radius + 1
     Xc = canvas_width - C
@@ -284,7 +290,7 @@ def get_start():
         Ti = int(Ti)
         
         if not (Xi < 0 or Xi >= canvas_width or Yi < 0 or Yi >= canvas_height):    # Check if the start node is within the canvas dimensions
-            if is_free(Xi, Yi):
+            if is_free(Xi, Yi, Ti):
                 break
             else:
                 print("Start node is inside an obstacle")
@@ -292,22 +298,22 @@ def get_start():
             print("Start node is out of bounds.")
     return Xi, Yi, Ti
 
+# Function to get the goal node
 def get_goal():
     C = clearance_distance + robo_radius + 1
     Xc = canvas_width - C
     Yc = canvas_height - C
     while True:
         if mode == '1':
-            print(f"\nThe start node and goal node should be within the canvas dimensions ({C}-{Xc}, {C}-{Yc}) and not inside an obstacle.\n")
+            print(f"\nThe goal node should be within the canvas dimensions ({C}-{Xc}, {C}-{Yc}) and not inside an obstacle.\n")
         Xg = input("Enter the goal node X: ")
         Yg = input("Enter the goal node Y: ")
-        To = input("Enter the goal node Angle: ")
+        # To = input("Enter the goal node Angle: ")
         Xg = int(Xg)
         Yg = int(Yg)
-        To = int(To)
-
+        To = 0
         if not (Xg < 0 or Xg >= canvas_width or Yg < 0 or Yg >= canvas_height):    # Check if the goal node is within the canvas dimensions
-            if is_free(Xg, Yg): 
+            if is_free(Xg, Yg, To): 
                 break
             else:
                 print("Goal node is inside an obstacle")
@@ -315,6 +321,7 @@ def get_goal():
             print("Goal node is inside an obstacle or out of bounds.")
     return Xg, Yg, To
 
+# Function to get the RPM values
 def get_rpm():
     while True:
         print("Enter the RPM values for the left and right wheels eg. 10, 20")
@@ -326,9 +333,9 @@ def get_rpm():
             break
     return RPM1, RPM2
 
-    
+# Check the mode selected by the user    
 if mode == '1':
-    print("\nGazebo mode selected.\n")
+    print("\nGazebo mode selected.\n")  # Since Gazebo mode is selected, the user does not need to enter the clearance distance, robot radius, RPM values, start node, and goal node
     clearance_distance = 2
     RPM1 = 10
     RPM2 = 20
@@ -338,9 +345,8 @@ if mode == '1':
     Yi = 95
     Ti = 0
     Xg, Yg, To = get_goal()
-
 else:
-    print("\n2D mode selected.\n")
+    print("\n2D mode selected.\n")   # Since 2D mode is selected, the user needs to enter the clearance distance, robot radius, RPM values, start node, and goal node
     clearance_distance, robo_radius = get_clearance()
     canvas, canvas_array = generate_map()
     RPM1, RPM2 = get_rpm()
@@ -348,7 +354,7 @@ else:
     Xg, Yg, To = get_goal()  
   
 
-out = cv2.VideoWriter('A_star.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (canvas_width * 2, canvas_height * 2 ))
+out = cv2.VideoWriter('A_star.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (canvas_width*2, canvas_height*2))
 
 Ti = Ti % 360
 Ti = round(Ti/30)*30                                          # Round the angle to the nearest multiple of 30
@@ -358,28 +364,21 @@ To = round(To/30)*30                                         # Round the angle t
 print("Start Node: ", (int(Xi), int(Yi), int(Ti)))
 print("Goal Node: ", (int(Xg), int(Yg), int(To)))
 print("\nCalculating the path...")
+
 Yi = abs(canvas_height - int(Yi))
 start_node = (int(Xi), int(Yi), int(Ti))
 
 Yg = abs(canvas_height - int(Yg))
 goal_node = (int(Xg), int(Yg), int(To))
 
-canvas_array = np.zeros((canvas_width, canvas_height))
-for x in range(canvas_width):
-    for y in range(canvas_height):
-        if all(canvas[y, x] != free_space_color):
-            canvas_array[x, y] = np.inf
-        else:
-            canvas_array[x, y] = 0
 
-cv2.circle(canvas, (Xi, Yi), 2, (0, 0, 255), -1)
-cv2.circle(canvas, (Xg, Yg), 2, (0, 255, 0), -1)
-canvas_resized = cv2.resize(canvas, (1200, 400), interpolation=cv2.INTER_AREA)
+cv2.circle(canvas, (Xi, Yi), 2, (0, 0, 255), -1) # Draw the start node
+cv2.circle(canvas, (Xg, Yg), 2, (0, 255, 0), -1) # Draw the goal node
+# canvas_resized = cv2.resize(canvas, (1200, 400), interpolation=cv2.INTER_AREA)
 for j in range(30):
-    out.write(canvas_resized)
+    out.write(canvas)
 
-
-start_time = time.time()
+start_time = time.time() # Start the timer
 came_from, cost_so_far, goal = a_star(start_node, goal_node)
 if came_from is None:
     print("No path found")
@@ -387,20 +386,20 @@ if came_from is None:
     execution_time = end_time - start_time         # Calculate the execution time
     print("Execution time: %.4f seconds" % execution_time)
     exit()
-path = reconstruct_path(came_from, start_node, goal)
-velocities = visualize_path(path)
+path = reconstruct_path(came_from, start_node, goal) # Reconstruct the path
+velocities = visualize_path(path) # Visualize the path
 
-end_time = time.time()
+end_time = time.time() # Stop the timer
 execution_time = end_time - start_time
 
-canvas_resized = cv2.resize(canvas, (1200, 400), interpolation=cv2.INTER_AREA)
+# canvas_resized = cv2.resize(canvas, (1200, 400), interpolation=cv2.INTER_AREA)
 for i in range(30):
-    out.write(canvas_resized)
-
+    out.write(canvas)
 out.release()
 print("Execution time: %.4f seconds" % execution_time)
-cv2.waitKey(0)
+cv2.waitKey(0) # Wait for the user to press any key
 
+# Publish the velocities to the robot
 def main(velocities):
     rclpy.init()
     velocity_publisher = VelocityPublisher()
@@ -417,7 +416,6 @@ def main(velocities):
         rclpy.shutdown()
 
 if __name__ == '__main__':
-    if mode == '1':
+    if mode == '1': # Publish the velocities only if Gazebo mode is selected
         main(velocities)
-
 cv2.destroyAllWindows()
